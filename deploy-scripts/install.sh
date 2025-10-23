623d84a812a01df4f786f6e74342d21913b6b85f
Prashanthsraju14 <90021377+Prashanthsraju14@users.noreply.github.com>
2025-10-23 05:34:12 +0000

    codebuild

--- deploy-scripts/install.sh
+++ deploy-scripts/install.sh
@@ -1,28 +1,27 @@
-#!/bin/bash
-dos2unix "$0"
-source /var/xigify/config
-cd /var/tempbuild/code/weather
-rm -rf deploy
-mkdir deploy
-dotnet publish -c Release -o deploy
-cd ..
-source /var/xigify/config
-sudo rm -rf /var/www/$XIGIFYENV/weather
-sudo mkdir -p /var/www/$XIGIFYENV/weather
-sudo mkdir -p /var/tempbuild/code
-sudo cp -a /var/tempbuild/code/weather/deploy/. /var/www/$XIGIFYENV/weather/
-sudo cp -a /var/tempbuild/code/weather/systemd/Xigify.weather.$XIGIFYENV.service /etc/systemd/system
-sudo cp -a /var/www/$XIGIFYENV/weather/weather.$XIGIFYENV.appsettings.json /var/www/$XIGIFYENV/weather/weather.appsettings.json 
-sudo systemctl daemon-reload
-sudo rm -rf /var/tempbuild/code/weather
-
-
+# cd /var/tempbuild/code/weather
+# rm -rf deploy
+# mkdir deploy
+# dotnet publish -c Release -o deploy
+# cd ..
 # source /var/xigify/config
 # sudo rm -rf /var/www/$XIGIFYENV/weather
 # sudo mkdir -p /var/www/$XIGIFYENV/weather
 # sudo mkdir -p /var/tempbuild/code
-# sudo cp -a /var/tempbuild/code/weather/output/. /var/www/$XIGIFYENV/weather/
+# sudo cp -a /var/tempbuild/code/weather/deploy/. /var/www/$XIGIFYENV/weather/
 # sudo cp -a /var/tempbuild/code/weather/systemd/Xigify.weather.$XIGIFYENV.service /etc/systemd/system
 # sudo cp -a /var/www/$XIGIFYENV/weather/weather.$XIGIFYENV.appsettings.json /var/www/$XIGIFYENV/weather/weather.appsettings.json 
 # sudo systemctl daemon-reload
 # sudo rm -rf /var/tempbuild/code/weather
+
+#!/bin/bash
+dos2unix "$0"
+source /var/xigify/config
+source /var/xigify/config
+sudo rm -rf /var/www/$XIGIFYENV/weather
+sudo mkdir -p /var/www/$XIGIFYENV/weather
+sudo mkdir -p /var/tempbuild/code
+sudo cp -a /var/tempbuild/code/weather/output/. /var/www/$XIGIFYENV/weather/
+sudo cp -a /var/tempbuild/code/weather/systemd/Xigify.weather.$XIGIFYENV.service /etc/systemd/system
+sudo cp -a /var/www/$XIGIFYENV/weather/weather.$XIGIFYENV.appsettings.json /var/www/$XIGIFYENV/weather/weather.appsettings.json 
+sudo systemctl daemon-reload
+sudo rm -rf /var/tempbuild/code/weather
